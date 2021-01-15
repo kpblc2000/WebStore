f6916881cad8ee51d6c5f3e0e8fe09115fa088a9
kpblc2000 <kpblc2000@gmail.com>
2021-01-15 20:38:41 +0300

    Замена класса на record; подключение статического содержимого

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -10,12 +11,23 @@ using System.Threading.Tasks;
 
 namespace WebStore
 {
-  public class Startup
+  public record Startup(IConfiguration Configuration)
   {
+
+    //private IConfiguration Configuration { get; }
+
+    //public Startup(IConfiguration Configuration)
+    //{
+
+    //}
+
+
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
+      //services.AddMvc
+      services.AddControllersWithViews();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,13 +38,20 @@ namespace WebStore
         app.UseDeveloperExceptionPage();
       }
 
+      app.UseStaticFiles();
+
       app.UseRouting();
 
+      //var greet = Configuration["Greetings"];
       app.UseEndpoints(endpoints =>
       {
+        endpoints.MapGet("/g", async context =>
+        {
+          await context.Response.WriteAsync("g");
+        });
         endpoints.MapGet("/", async context =>
               {
-            await context.Response.WriteAsync("Hello World!");
+            await context.Response.WriteAsync(Configuration["Greetings"]);
           });
       });
     }
