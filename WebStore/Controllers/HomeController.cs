23094dc3d585dd6fef271e3a850d0365616968fc
kpblc2000 <kpblc2000@gmail.com>
2021-01-15 21:11:20 +0300

    Представление сотрудников

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
   public class HomeController : Controller
   {
+
+    private static readonly List<Employee> _Employees = new()
+    {
+      new Employee { Id = 1, FirstName = "f1", LastName = "l1", Patronymic = "p1", YearBirst = 1990 },
+      new Employee { Id = 1, FirstName = "f2", LastName = "l2", Patronymic = "p2", YearBirst = 1991 },
+      new Employee { Id = 1, FirstName = "f3", LastName = "l3", Patronymic = "p3", YearBirst = 1989 },
+    };
+
     public IActionResult Index()
     {
       //return View();
@@ -15,5 +25,11 @@ namespace WebStore.Controllers
     {
       return Content("Second Controller Action");
     }
+
+    public IActionResult Employees()
+    {
+      return View(_Employees);
+    }
+
   }
 }
