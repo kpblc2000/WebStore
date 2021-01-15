1fecfa0c806208202b2c13b8de527319b15645f3
kpblc2000 <kpblc2000@gmail.com>
2021-01-15 20:24:16 +0300

    initial commit

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -3,15 +3,17 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebStore
 {
   public class Startup
   {
+
+    public Startup()
+    {
+
+    }
+
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
@@ -32,8 +34,8 @@ namespace WebStore
       {
         endpoints.MapGet("/", async context =>
               {
-            await context.Response.WriteAsync("Hello World!");
-          });
+                await context.Response.WriteAsync("Hello World!");
+              });
       });
     }
   }
